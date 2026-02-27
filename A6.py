9cac83e03685dc71496da49f282e1e00e9108927
Z-thrones <zaviyar.ktk@gmail.com>
2026-02-27 14:37:37 +0500

    Add files via upload

---
+++ A6.py
@@ -0,0 +1,7 @@
+sentence1 = "I am having a great day"
+sentence2 = ",Looking forward to having a pleasent year too"
+print(sentence1)
+print(sentence2)
+print("Combined:")
+joined = sentence1 + sentence2
+print("Answer is:" , joined)
